ffd5e2c128bb308ee5cf41c9e4ece5b4a005ed37
MiBoCun <Midadada@aliyun.com>
2020-09-04 22:19:26 +0800

    week004 code format

--- Week_04/455. 分发饼干.js
+++ Week_04/455. 分发饼干.js
@@ -9,8 +9,8 @@ var findContentChildren = function(g, s) {
     let i=0; //学生
     let j=0; //饼干
     let count=0;
-    while(i<=sortG.length-1 && j<=sortS.length-1){
-          if(sortS[j]>=sortG[i]){
+    while (i<=sortG.length-1 && j<=sortS.length-1) {
+          if (sortS[j]>=sortG[i]) {
               count++
               i++              
           }
