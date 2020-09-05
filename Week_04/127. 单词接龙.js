75245cf97c13eb72c44cb174b20d2725587b53d8
MiBoCun <Midadada@aliyun.com>
2020-09-05 11:28:25 +0800

    change visited to Set

--- Week_04/127. 单词接龙.js
+++ Week_04/127. 单词接龙.js
@@ -1,10 +1,7 @@
     var ladderLength = function(beginWord, endWord, wordList) {
-
         let len=beginWord.length;
-        let map=new Map() //保存所有单词的所有可能的连接单词
-        let queue=[[beginWord,1]]  //记录访问路径
-        let visited=new Map()  //存储访问过的单词
-        visited.set(beginWord,true)
+        let map=new Map() //保存所有单词的所有可能的连接单词      
+       
         for (let w of wordList) {
             for(let i=0;i<len;i++) {
                 let newW=w.substring(0,i)+'*'+w.substring(i+1,len)
@@ -13,6 +10,9 @@
                     map.set(newW,mapItem)
                }
         }
+        let queue=[[beginWord,1]]  //记录访问路径
+        let visited=new Set()  //存储访问过的单词
+            visited.add(beginWord)
         while (queue.length){
             let curNode=queue.shift()
             let curW=curNode[0]
@@ -22,13 +22,13 @@
                 let qWord=curW.substring(0,i)+'*'+curW.substring(i+1,len)
                 if (map.has(qWord)) {
                     let tempW=map.get(qWord)
-                    for (let i=0;i<tempW.length;i++) {
-                        if (tempW[i]===endWord) {
+                    for (let tem of tempW) {
+                        if (tem===endWord) {
                             return curL+1
                         }
-                        if (!visited.has(tempW[i])) {
-                            visited.set(tempW[i],true)
-                            queue.push([tempW[i],curL+1])
+                        if (!visited.has(tem)) {
+                            visited.add(tem)
+                            queue.push(tem,curL+1])
                         }
                     }
                 }
