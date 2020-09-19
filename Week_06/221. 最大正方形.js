6d98f88983ec7f4b3c6466956f38063a41b3cfd6
MiBoCun <Midadada@aliyun.com>
2020-09-19 21:02:23 +0800

    week-06 1:add title description 2:add summary of dynamic programming

--- Week_06/221. 最大正方形.js
+++ Week_06/221. 最大正方形.js
@@ -1,7 +1,16 @@
-/**
- * @param {character[][]} matrix
- * @return {number}
- */
+/* 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+示例:
+
+输入: 
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4 */
+
+
 var maximalSquare = function(matrix) {
     if ( matrix.length == 0 ) {
               return 0;
